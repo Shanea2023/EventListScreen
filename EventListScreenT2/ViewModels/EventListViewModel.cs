1e971469d3df992120fd8071e5c6779abba5c032
Shanea2023 <chapotererashanea@gmail.com>
2026-03-02 11:12:01 +0700

    Create ViewModels folder and EventListViewModel class

---
+++ EventListScreenT2/ViewModels/EventListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventListScreenT2.ViewModels
+{
+    internal class EventListViewModel
+    {
+    }
+}
