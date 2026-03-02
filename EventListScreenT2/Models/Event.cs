5ff691a2b3b3993398a3f2f866688c1f1cbf0ffa
DavidHE <davidhoangem@gmail.com>
2026-03-02 10:25:30 +0700

    Add Event model with required properties

---
+++ EventListScreenT2/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace EventListScreenT2.Models;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public string Image { get; set; } 
+    public string Category { get; set; }
+    public bool IsRegistered { get; set; }
+}
