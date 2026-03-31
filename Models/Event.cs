07bf152a246217a91fca590ea129f628785a0d12
DavidHE <davidhoangem@gmail.com>
2026-03-31 11:25:19 +0700

    Task 1: HoangEm da tao xong Model Event

---
+++ Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Campus.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+        public string ImageUrl { get; set; } = "dotnet_bot.png";
+        public string Category { get; set; } = "General"; 
+        public bool IsRegistered { get; set; }
+    }
+}
